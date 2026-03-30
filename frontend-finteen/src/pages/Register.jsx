dfaf09e1a39b7253d00b6f94d1161455ec324f7b
Achmdysf <amongus930932@gmail.com>
2026-03-30 10:01:58 +0700

    Update Register.jsx

--- frontend-finteen/src/pages/Register.jsx
+++ frontend-finteen/src/pages/Register.jsx
@@ -110,7 +110,7 @@ export default function Register() {
   };
 
   const handleGoogleRegister = () => {
-    const apiUrl = import.meta.env.VITE_API_URL || "http://localhost:9000/api";
+    const apiUrl = import.meta.env.VITE_API_URL || "https://capstone-finteen-production.up.railway.app/";
     window.location.href = `${apiUrl}/auth/google?state=register`;
   };
 
