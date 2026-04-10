66e5bdebffee0071dbc7c7c977192ef8257ed217
Faruq Yeeay <faruq@Faruqs-MacBook-Air.local>
2026-04-10 11:12:35 +0700

    tes email

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,25 +1,20 @@
-const { Resend } = require('resend');
-
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 const sendEmail = async ({ email, subject, message }) => {
   console.log(`📧 Mengirim email ke: ${email}`);
-  console.log(`📧 Subject: ${subject}`);
 
   try {
-    const response = await resend.emails.send({
-      from: process.env.FROM_EMAIL, // contoh: onboarding@resend.dev
+    const info = await transporter.sendMail({
+      from: `"FinTeen" <${process.env.SMTP_EMAIL}>`,
       to: email,
       subject: subject,
       html: message,
     });
 
-    console.log("✅ Email berhasil dikirim:", response);
-    return response;
+    console.log("✅ Email berhasil:", info.response);
+    return info;
 
   } catch (error) {
-    console.error("❌ Gagal mengirim email:", error);
-    throw new Error("Email gagal dikirim");
+    console.error("❌ Error email:", error);
+    throw error;
   }
 };
 
